6bc2a900d33f13e3be1b736493fcc4fb01007ca8
tian <xiaoliang.tian@gmail.com>
2018-06-13 18:19:57 +0800

    fix bugs

--- perf/client/client.go
+++ perf/client/client.go
@@ -8,10 +8,14 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		resp, _ := http.Get(os.Getenv("TARGET"))
+		resp, err := http.Get(os.Getenv("TARGET"))
+		if err != nil {
+			w.WriteHeader(500)
+		}
 		log.Println(resp)
 		b := make([]byte, 0)
 		_, _ = resp.Body.Read(b)
+		resp.Body.Close()
 		w.Write(b)
 
 	})
--- perf/server-io/server.go
+++ perf/server-io/server.go
@@ -9,6 +9,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		resp, _ := http.Get("http://127.0.0.1:9999/")
 		log.Println(resp)
+		resp.Body.Close()
 		w.Write([]byte("hello"))
 
 	})
