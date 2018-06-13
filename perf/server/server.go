46a51bd5cf8cf840edab8c758e2f44169821bcd9
tian <xiaoliang.tian@gmail.com>
2018-06-13 11:59:38 +0800

    k8s spec

--- perf/server/server.go
+++ perf/server/server.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"net/http"
+	"fmt"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("hello")
 		w.Write([]byte("hello"))
 
 	})
